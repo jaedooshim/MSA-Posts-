fc322fc0017747c12cb07a98aaf56ceb792d4387
shimjaedoo <woen1637@naver.com>
2024-03-31 21:17:47 +0900

    chore : build

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { HttpExceptionFilter } from './exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 5000,
+      },
+    },
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
+  await app.listen();
+}
+bootstrap();
